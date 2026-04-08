3189da1ffcf25605bda3d17d9c7a1bb91d921755
David Alade <david.alade2004@gmail.com>
2026-04-08 20:46:40 +0100

    Reverted to an hour ago

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 An AI-powered study assistant that turns lecture content into active learning by generating questions, evaluating answers, identifying mistakes, and recommending personalized revision.
 
-Link to Streamlit Prototype
+
 
--- frontend/pages/progress.py
+++ frontend/pages/progress.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import plotly.graph_objects as go
-
 def show_progress():
     
     history=st.session_state.get("quiz_history",[])
@@ -11,23 +10,32 @@ def show_progress():
 
     attempt_numbers=list(range(1,len(history)+1))
     percentages=[attempt["percentage"] for attempt in history]
-
     percentages.reverse()
     fig = go.Figure()
+    theme = st.session_state.get("theme", "Dark") 
+    if theme=="Light":
+        line=dict(color='navy', width=5)
+        marker=dict(size=10, color='black')
+    else:
+        line=dict(color='#ADD8E6', width=5)
+        marker=dict(size=10, color='white')
+   
     fig.add_trace(go.Scatter(
         x=attempt_numbers,
         y=percentages,
         mode='lines+markers',
-        line=dict(color='navy', width=5),
-        marker=dict(size=10, color='black'),
+        line=line,
+        marker=marker,
         hovertemplate='Attempt %{x}: %{y}%<extra></extra>'
     ))
 
-    fig.update_layout(
-        title=dict(
+    
+
+    if theme=="Light":
+        title = dict(
             text="Quiz Progress",
             font=dict(color="black", size=24)
-        ),
+        )
         xaxis=dict(
             title=dict(
                 text="Attempts",
@@ -35,7 +43,7 @@ def show_progress():
             ),
             tickfont=dict(color="grey", size=14),
             dtick=1
-        ),
+        )
         yaxis=dict(
             title=dict(
                 font=dict(color="navy", size=18)
@@ -43,13 +51,38 @@ def show_progress():
             tickfont=dict(color="grey", size=14),
             range=[0, 100],
             dtick=10
-    ),
+    )
+    else:
+        title = dict(
+            text="Quiz Progress",
+            font=dict(color="#e5ecf6", size=24)
+        )
+        xaxis = dict(
+            title=dict(
+                text="Attempts",
+                font=dict(color="#e5ecf6", size=18)
+            ),
+            tickfont=dict(color="#94a3b8", size=14),
+            dtick=1
+        )
+        yaxis = dict(
+            title=dict(
+                font=dict(color="#e5ecf6", size=18)
+            ),
+            tickfont=dict(color="#94a3b8", size=14),
+            range=[0, 100],
+            dtick=10
+        )
+    fig.update_layout(
+        title=title,
+        xaxis=xaxis,
+        yaxis=yaxis,
         width=600,
         height=400,
         template="simple_white",
         paper_bgcolor='rgba(0,0,0,0)',
         plot_bgcolor='rgba(0,0,0,0)'
-    )
+)
     current_index=-1
     fig.add_annotation(
     x=attempt_numbers[current_index],
@@ -80,22 +113,30 @@ def show_progress():
             max_score = last
             st.info("Only one attempt recorded.")
 
-        if last> max_score:
-            st.markdown(f"<h1 style='font-size:50px; color:green;'>NEW HIGH SCORE! {last}%</h1>", unsafe_allow_html=True)
+        if last > max_score:
+            st.markdown(
+                f'<div style="color:green; font-size:50px; font-weight:bold;">NEW HIGH SCORE! {last}%</div>',
+                unsafe_allow_html=True
+            )
         else:
-            st.markdown(f"<h1 style='font-size:30px; color:blue;'>Current Score: {last}%</h1>", unsafe_allow_html=True)
-            st.markdown(f"<h2 style='font-size:20px; color:orange;'>High Score: {max_score}% Can you beat it?</h2>", unsafe_allow_html=True)
-
+            st.markdown(
+                f'<div style="color:blue; font-size:30px;">Current Score: {last}%</div>',
+                unsafe_allow_html=True
+            )
+            st.markdown(
+                f'<div style="color:orange; font-size:20px;">High Score: {max_score}%</div>',
+                unsafe_allow_html=True
+            )
         history = st.session_state.get("quiz_history", [])
         if history:
             last_attempt = history[0]  # newest attempt is at index 0
             wrong_questions = last_attempt.get("wrong_questions", [])
+            with st.expander("**Suggested Next Topics**", expanded=False):
 
-            if wrong_questions:
-                st.markdown("### *Suggested Next Topics*")
-                for q in wrong_questions:
-                    st.markdown(f"- {q}")
-            else:
-                st.markdown("🎉 All correct! No suggested topics.")
+                if wrong_questions:
+                    for q in wrong_questions:
+                        st.markdown(f"- {q}")
+                else:
+                    st.markdown("🎉 All correct! No suggested topics.")
         else:
             st.info("No quiz attempts yet.")
